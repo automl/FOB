284699d7cb5ab6cb7ed3f57f72b167b9e98c2326
Simon Blauth <simon.blauth@mailbox.org>
2023-11-30 16:41:13 +0100

    basic skeleton for submission and workload API

---
+++ bob/workloads/template/workload.py
@@ -0,0 +1,11 @@
+# impoorts
+
+def get_datamodule():
+    pass
+
+def get_model():
+    pass
+
+# auxilliary information like batch_size, max_steps, etc...
+def get_specs():
+    pass
---
+++ submissions/template/submission.py
@@ -0,0 +1,4 @@
+# imports
+
+def configure_optimizers():
+    pass
