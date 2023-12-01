4a21f3d75a87a7795593f76d58ef0bd2c9dac32f
Zacharias Haeringer <haeringz@informatik.uni-freiburg.de>
2023-12-01 15:00:12 +0100

    mnist model

--- workloads/mnist/model.py
+++ workloads/mnist/model.py
@@ -6,24 +6,46 @@ class MNISTModel(LightningModule):
     def __init__(self, create_optimizer_fn: Callable):
         super().__init__()
         self.create_optimizer_fn = create_optimizer_fn
-        # TODO create model
-        # self.model = torch.nn.Sequential(
-        #     torch.nn.Linear(1, 10),
-        #     torch.nn.ReLU(),
-        #     torch.nn.Linear(10, 1),
-        #     torch.nn.ReLU(),
-        # )
+
+        input_size = 28 * 28
+        num_hidden = 128
+        num_classes = 10
+        
+        # TODO param?
+        lr_rate = 1.0
+        self.lr_rate = lr_rate
+        
+        # algoperf net
+        # https://github.com/mlcommons/algorithmic-efficiency/blob/main/algorithmic_efficiency/workloads/mnist/mnist_pytorch/workload.py
+        self.model = torch.nn.Sequential(
+            torch.nn.Linear(input_size, num_hidden, bias=True),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(num_hidden, num_classes, bias=True),
+        )
+        # negative log likelihood loss
+        self.loss = torch.nn.functional.nll_loss
+
+    def forward(self, x):
+        batch_size, channels, widht, height = x.size()
+
+        # (b, 1, 28, 28) -> (b, 1*28*28)
+        x = x.view(batch_size, -1)
+        output = self.model(x)
+        prediction = torch.softmax(output, dim=1)
+        return prediction
+
 
     def training_step(self, batch, batch_idx):
-        # training_step defines the train loop.
-        # it is independent of forward
-        # x = y = batch
-        # y_hat = self.model(x)
-        # loss = torch.nn.functional.mse_loss(y_hat, y)
+        x, y = batch
+        y_hat = self.model(x)
+        loss = self.loss(y_hat, y)
+
+        logs = {'train_loss': loss}
+        return {'loss': loss, 'log': logs}
+    
         # # Logging to TensorBoard (if installed) by default
         # self.log("train_loss", loss)
         # return loss
-        ...
 
     def configure_optimizers(self):
         return self.create_optimizer_fn(self)
