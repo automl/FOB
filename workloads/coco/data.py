ddd575c6852b1405b7eca5a70a86b4a7ff15433e
Simon Blauth <simon.blauth@mailbox.org>
2023-12-18 21:22:42 +0100

    working coco model for training, eval still missing

--- workloads/coco/data.py
+++ workloads/coco/data.py
@@ -21,16 +21,22 @@ class COCODataModule(WorkloadDataModule):
     def __init__(self, runtime_args: RuntimeArgs):
         super().__init__(runtime_args)
         self.data_dir = self.data_dir / "COCO"
-        self.batch_size = 2
-        self.transforms = v2.Compose(
+        self.batch_size = 8
+        self.train_transforms = v2.Compose(
             [
                 v2.ToImage(),
-                v2.RandomPhotometricDistort(p=1),
-                v2.RandomZoomOut(fill={tv_tensors.Image: (123, 117, 104), "others": 0}),
-                v2.RandomIoUCrop(),
-                v2.RandomHorizontalFlip(p=1),
-                v2.SanitizeBoundingBoxes(),
+                v2.RandomHorizontalFlip(p=0.5),
                 v2.ToDtype(torch.float32, scale=True),
+                v2.ConvertBoundingBoxFormat(tv_tensors.BoundingBoxFormat.XYXY),
+                v2.SanitizeBoundingBoxes(),
+                v2.ToPureTensor(),
+            ]
+        )
+        self.val_transforms = v2.Compose(
+            [
+                v2.ToImage(),
+                v2.ToDtype(torch.float32, scale=True),
+                v2.ToPureTensor(),
             ]
         )
         self.collate_fn = lambda batch: tuple(zip(*batch))
@@ -93,13 +99,15 @@ class COCODataModule(WorkloadDataModule):
         val_path = self.data_dir / "val2017"
         annot_path = self.data_dir / "annotations"
         if stage == "fit":
-            self.data_train = self._wrapped_coco_dataset(train_path, annot_path / "instances_train2017.json")
-            self.data_val = self._wrapped_coco_dataset(val_path, annot_path / "instances_val2017.json")
+            self.data_train = self._coco_remove_images_without_annotations(
+                self._wrapped_coco_dataset(train_path, annot_path / "instances_train2017.json", self.train_transforms)
+            )
+            self.data_val = self._wrapped_coco_dataset(val_path, annot_path / "instances_val2017.json", self.val_transforms)
         # use validation set for test and predict, because test labels are not available
         if stage == "test":
-            self.data_test = self._wrapped_coco_dataset(val_path, annot_path / "instances_val2017.json")
+            self.data_test = self._wrapped_coco_dataset(val_path, annot_path / "instances_val2017.json", self.val_transforms)
         if stage == "predict":
-            self.data_predict = self._wrapped_coco_dataset(val_path, annot_path / "instances_val2017.json")
+            self.data_predict = self._wrapped_coco_dataset(val_path, annot_path / "instances_val2017.json", self.val_transforms)
 
     def train_dataloader(self) -> DataLoader:
         return self._dataloader_from_dataset(self.data_train)
@@ -115,18 +123,60 @@ class COCODataModule(WorkloadDataModule):
 
     def _dataloader_from_dataset(self, dataset: Dataset) -> DataLoader:
         self.check_dataset(dataset)
-        return DataLoader(dataset, batch_size=self.batch_size, collate_fn=self.collate_fn)
+        # TODO: batch_size=1 for val/test?
+        return DataLoader(dataset, batch_size=self.batch_size, num_workers=self.workers, collate_fn=self.collate_fn)
 
 
     def get_specs(self) -> dict[str, Any]:
         return {"batch_size": self.batch_size}
 
-    def _wrapped_coco_dataset(self, imgs: Path, annot: Path) -> Dataset:
-        ds = CocoDetection(str(imgs), str(annot), transforms=self.transforms)
-        return datasets.wrap_dataset_for_transforms_v2(ds, target_keys=["boxes", "labels", "masks"])
+    def _wrapped_coco_dataset(self, imgs: Path, annot: Path, transforms: v2.Compose) -> CocoDetection:
+        ds = CocoDetection(str(imgs), str(annot), transforms=transforms)
+        return datasets.wrap_dataset_for_transforms_v2(ds, target_keys=["boxes", "labels", "image_id"])
 
     def _coco_img_link(self, mode: str) -> str:
         return f"http://images.cocodataset.org/zips/{mode}2017.zip"
 
     def _coco_annot_link(self) -> str:
         return "http://images.cocodataset.org/annotations/annotations_trainval2017.zip"
+
+    def _coco_remove_images_without_annotations(self, dataset: CocoDetection, cat_list=None):
+        """
+        Implementation taken from https://github.com/pytorch/vision/blob/main/references/detection/coco_utils.py
+        """
+        def _has_only_empty_bbox(anno):
+            return all(any(o <= 1 for o in obj["bbox"][2:]) for obj in anno)
+
+        def _count_visible_keypoints(anno):
+            return sum(sum(1 for v in ann["keypoints"][2::3] if v > 0) for ann in anno)
+
+        min_keypoints_per_image = 10
+
+        def _has_valid_annotation(anno):
+            # if it's empty, there is no annotation
+            if len(anno) == 0:
+                return False
+            # if all boxes have close to zero area, there is no annotation
+            if _has_only_empty_bbox(anno):
+                return False
+            # keypoints task have a slight different criteria for considering
+            # if an annotation is valid
+            if "keypoints" not in anno[0]:
+                return True
+            # for keypoint detection tasks, only consider valid images those
+            # containing at least min_keypoints_per_image
+            if _count_visible_keypoints(anno) >= min_keypoints_per_image:
+                return True
+            return False
+
+        ids = []
+        for ds_idx, img_id in enumerate(dataset.ids):
+            ann_ids = dataset.coco.getAnnIds(imgIds=img_id, iscrowd=None)
+            anno = dataset.coco.loadAnns(ann_ids)
+            if cat_list:
+                anno = [obj for obj in anno if obj["category_id"] in cat_list]
+            if _has_valid_annotation(anno):
+                ids.append(ds_idx)
+
+        dataset = torch.utils.data.Subset(dataset, ids)
+        return dataset
--- workloads/coco/model.py
+++ workloads/coco/model.py
@@ -8,20 +8,36 @@ from submissions import Submission
 
 class COCODetectionModel(WorkloadModel):
     def __init__(self, submission: Submission):
-        model = fasterrcnn_mobilenet_v3_large_fpn()
+        model = fasterrcnn_mobilenet_v3_large_fpn(num_classes=91)
         super().__init__(model, submission)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.model(x)
+    def forward(self, x):
+        imgs, targets = x
+        return self.model(imgs, targets)
 
-    def training_step(self, batch, batch_idx) -> torch.Tensor:
-        return torch.rand(batch.size())
+    def training_step(self, batch, batch_idx):
+        imgs, targets = batch
+        loss_dict = self.model(imgs, targets)
+        self.log_losses(loss_dict, "train")
+        return self.total_loss(loss_dict)
 
     def validation_step(self, batch, batch_idx):
-        pass
+        imgs, targets = batch
+        self.model(imgs, targets)
+        # TODO: calculate metrics
 
     def test_step(self, batch, batch_idx):
+        # no test labels available for coco
         pass
 
+    def log_losses(self, losses: dict, stage: str):
+        for loss, val in losses.items():
+            self.log(f"{stage}_{loss}", val)
+        total = sum(loss for loss in losses.values())
+        self.log(f"{stage}_loss_total", total)
+
+    def total_loss(self, losses: dict):
+        return sum(loss for loss in losses.values())
+
     def get_specs(self) -> dict[str, Any]:
-        return {"max_epochs": 1}
+        return {"max_epochs": 10}
