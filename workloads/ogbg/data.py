1f4fd57e9c780ea1dea1f5eebc4afdae1efe686e
Zacharias Haeringer <haeringz@informatik.uni-freiburg.de>
2024-01-16 09:49:24 +0100

    manual implementation for gat (wip)

--- workloads/ogbg/data.py
+++ workloads/ogbg/data.py
@@ -1,6 +1,9 @@
+# https://github.com/Diego999/pyGAT/blob/master/utils.py
+
+import numpy as np
+import scipy.sparse as sp
 import torch
 from torch.utils.data import DataLoader
-# from torchvision import transforms
 from workloads import WorkloadDataModule
 from runtime import DatasetArgs
 
@@ -17,13 +20,89 @@ class OGBGDataModule(WorkloadDataModule):
         # self.transform  = transforms.Compose([transforms.ToTensor(),transforms.Normalize(meanOfOpenWebText, stdOfOpenWebText)])  
         
     def prepare_data(self):
-        pass
+        """Load citation network dataset (cora only for now)"""
+        dataset = "cora"
+        self.data_dir = self.data_dir
+        print(f"Loading {dataset} dataset...")
+        if dataset == "cora":
+            labels_file =  self.data_dir / "cora.content"
+            edges_file =  self.data_dir / "cora.cites"
+
+            idx_features_labels = np.genfromtxt(labels_file, dtype=np.dtype(str))
+            features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+            labels = encode_onehot(idx_features_labels[:, -1])
+
+            # build graph
+            idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+            idx_map = {j: i for i, j in enumerate(idx)}
+            edges_unordered = np.genfromtxt(edges_file, dtype=np.int32)
+            edges = np.array(list(map(idx_map.get, edges_unordered.flatten())), dtype=np.int32).reshape(edges_unordered.shape)
+            adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])), shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
+
+            # build symmetric adjacency matrix
+            adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+            features = normalize_features(features)
+            adj = normalize_adj(adj + sp.eye(adj.shape[0]))
+
+            split_train = range(140)
+            split_val = range(200, 500)
+            split_test = range(500, 1500)
+
+            adj = torch.FloatTensor(np.array(adj.todense()))
+            features = torch.FloatTensor(np.array(features.todense()))
+            labels = torch.LongTensor(np.where(labels)[1])
+
+            idx_train = torch.LongTensor(idx_train)
+            idx_val = torch.LongTensor(idx_val)
+            idx_test = torch.LongTensor(idx_test)
+
 
     def setup(self, stage: str):
+        """setup is called from every process across all the nodes. Setting state here is recommended.
+        """
         if stage == "fit":
-            pass
+            self.data_train = ...
+        if stage == "validate":
+            self.data_val = ...
         if stage == "test":
-            pass
+            self.data_test = ...
         if stage == "predict":
-            pass
+            self.data_predict = ...
     
+
+
+
+def encode_onehot(labels):
+    # The classes must be sorted before encoding to enable static class encoding.
+    # In other words, make sure the first class always maps to index 0.
+    classes = sorted(list(set(labels)))
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
+    return labels_onehot
+
+
+def normalize_adj(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    r_inv_sqrt[np.isinf(r_inv_sqrt)] = 0.
+    r_mat_inv_sqrt = sp.diags(r_inv_sqrt)
+    return mx.dot(r_mat_inv_sqrt).transpose().dot(r_mat_inv_sqrt)
+
+
+def normalize_features(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double()
+    correct = correct.sum()
+    return correct / len(labels)
--- workloads/ogbg/model.py
+++ workloads/ogbg/model.py
@@ -1,15 +1,55 @@
+# from https://github.com/Diego999/pyGAT/blob/master/train.py
+
 import torch
 from workloads import WorkloadModel
 from runtime.specs import RuntimeSpecs
 from submissions import Submission
+from gnn import GAT, SpGAT
 
 class OGBGModel(WorkloadModel):
+    """GAT https://arxiv.org/pdf/1710.10903.pdf, implementation from https://github.com/Diego999/pyGAT/"""
     def __init__(self, submission: Submission):
-        
-        # TODO: decide which model to use; depends on the task of the model?
-        model = None
-        super().__init__(model, submission)
+        cuda = True
+        sparse: bool = False  # GAT with sparse version or not
+        nfeat: int = 0  # TODO adapt for dataset
+        hidden: int = 8  # Number of hidden units
+        nclass: int = 0  # TODO adapt for dataset
+        dropout: float = 0.6 # Dropout rate (1 - keep probability)
+        nb_heads: int = 8  # Number of head attentions
+        alpha: float = 0.2  # Alpha for the leaky_relu
 
+        if sparse:
+            model = SpGAT(
+                nfeat=nfeat,
+                nhid=hidden, 
+                nclass=nclass, 
+                dropout=dropout, 
+                nheads=nb_heads, 
+                alpha=alpha)
+        else:
+            model = GAT(
+                nfeat=nfeat, 
+                nhid=hidden, 
+                nclass=nclass, 
+                dropout=dropout, 
+                nheads=nb_heads, 
+                alpha=alpha)
+
+        # TODO: port to lightning
+        """
+        # Load data
+        adj, features, labels, idx_train, idx_val, idx_test = load_data()
+        if cuda:
+            model.cuda()
+            features = features.cuda()
+            adj = adj.cuda()
+            labels = labels.cuda()
+            idx_train = idx_train.cuda()
+            idx_val = idx_val.cuda()
+            idx_test = idx_test.cuda()
+        """
+        super().__init__(model, submission)
+        loss = torch.nn.CrossEntropyLoss
         # datasets:
         #   ogbg-molhiv
         #   https://ogb.stanford.edu/docs/graphprop/
@@ -18,12 +58,32 @@ class OGBGModel(WorkloadModel):
         # metric:
         #   ROC-AUC
         # self.loss_fn = None # TODO
+        self.loss_fn = torch.nn.functional.nll_loss
+
+
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
 
     def training_step(self, batch, batch_idx) -> torch.Tensor:
-        raise NotImplementedError
+        # optimizer.zero_grad()  # Clear gradients.
+        
+        # TODO batch depends on how we load data
+        # idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
+        # features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+        # labels = encode_onehot(idx_features_labels[:, -1])
+        # adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])), shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
+        # build symmetric adjacency matrix
+        # adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+        # features = normalize_features(features)
+        # adj = normalize_adj(adj + sp.eye(adj.shape[0]))
+        
+        output = self.model(features, adj)
+        loss_train = self.loss_fn(output[idx_train], labels[idx_train])
+        self.loss_fn.backward()  # Derive gradients.
+        # optimizer.step()  # Update parameters based on gradients.
+        return loss, h
 
     def validation_step(self, batch, batch_idx):
         raise NotImplementedError
