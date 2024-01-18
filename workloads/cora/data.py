a61522f1e806e4c63dc827a8824e4eec85c9bd73
Zacharias Haeringer <haeringz@informatik.uni-freiburg.de>
2024-01-18 12:16:16 +0100

    downlading cora set, wip model

--- workloads/cora/data.py
+++ workloads/cora/data.py
@@ -1,4 +1,6 @@
 import torch
+import torch_geometric.loader as geom_loader
+import torch_geometric.nn as geom_nn
 from torch_geometric.datasets import Planetoid
 from torch_geometric.transforms import NormalizeFeatures
 from torch.utils.data import DataLoader
@@ -6,20 +8,54 @@ from workloads import WorkloadDataModule
 from runtime import DatasetArgs
 
 class CoraDataModule(WorkloadDataModule):
-    """https://colab.research.google.com/drive/14OvFnAXggxB8vM4e8vSURUp1TaKnovzX?usp=sharing#scrollTo=imGrKO5YH11-"""
+    """https://colab.research.google.com/drive/14OvFnAXggxB8vM4e8vSURUp1TaKnovzX?usp=sharing#scrollTo=imGrKO5YH11-
+    https://pytorch-geometric.readthedocs.io/en/latest/notes/introduction.html?highlight=planetoid#common-benchmark-datasets
+    https://lightning.ai/docs/pytorch/stable/notebooks/course_UvA-DL/06-graph-neural-networks.html"""
     def __init__(self, dataset_args: DatasetArgs):
         super().__init__(dataset_args)
-        
-    def prepare_data(self):
-        """Load citation network dataset (cora only for now)"""
-        self.data_dir = self.data_dir
-        dataset = Planetoid(root='data/Planetoid', name='Cora', transform=NormalizeFeatures())
-        print(f'Dataset: {dataset}:')
-        print('======================')
-        print(f'Number of graphs: {len(dataset)}')
-        print(f'Number of features: {dataset.num_features}')
-        print(f'Number of classes: {dataset.num_classes}')
+        self.batch_size = 1  # As we have a single graph, we use a batch size of 1
+        self.data_dir = self.data_dir / "Planetoid"
 
+
+    def prepare_data(self):
+        """Load citation network dataset (cora)"""
+        self.data_dir.mkdir(exist_ok=True)
+
+        # dataset split:
+        # https://pytorch-geometric.readthedocs.io/en/latest/generated/torch_geometric.datasets.Planetoid.html
+        # The type of dataset split ("public", "full", "geom-gcn", "random").
+        # If set to "public", the split will be the public fixed split from the “Revisiting Semi-Supervised Learning with Graph Embeddings” paper.
+        # If set to "full", all nodes except those in the validation and test sets will be used for training (as in the “FastGCN: Fast Learning with Graph Convolutional Networks via Importance Sampling” paper).
+        # If set to "geom-gcn", the 10 public fixed splits from the “Geom-GCN: Geometric Graph Convolutional Networks” paper are given.
+        # If set to "random", train, validation, and test sets will be randomly generated, according to num_train_per_class, num_val and num_test. (default: "public")
+        split = 'public'
+        dataset = Planetoid(root=self.data_dir, name='Cora', split=split, transform=NormalizeFeatures())
+
+        print_cora_stats = True
+        if print_cora_stats:
+            print(f'Dataset: {dataset}:')
+            print('======================')
+            print(f'  Number of graphs: {len(dataset)}')
+            print(f'  Number of features: {dataset.num_features}')
+            print(f'  Number of classes: {dataset.num_classes}')
+            data = dataset[0]
+            print('  --------------------')
+            print(f'  Number of nodes: {data.num_nodes}')
+            print(f'  Number of edges: {data.num_edges}')
+            print(f'  Average node degree: {data.num_edges / data.num_nodes:.2f}')
+            print(f'  Number of training nodes: {data.train_mask.sum()}')
+            print(f'  Training node label rate: {int(data.train_mask.sum()) / data.num_nodes:.2f}')
+            print(f'  Has isolated nodes: {data.has_isolated_nodes()}')
+            print(f'  Has self-loops: {data.has_self_loops()}')
+            print(f'  Is undirected: {data.is_undirected()}')
+        
+        # share the same data loader for the train, validation, and test set
+        # (the mask is picked inside the Lightning module)
+        loader = geom_loader.DataLoader(dataset, batch_size=self.batch_size)
+        self.data_train = loader
+        self.data_val = loader
+        self.data_test = loader
+        # self.data_predict = loader
 
 
     def setup(self, stage: str):
--- workloads/cora/model.py
+++ workloads/cora/model.py
@@ -3,33 +3,54 @@ from workloads import WorkloadModel
 from runtime.specs import RuntimeSpecs
 from submissions import Submission
 from torch_geometric.nn import GCNConv
+import torch.nn.functional as F
 
 
 class CoraModel(WorkloadModel):
     """GAT from pytorch geometric"""
     def __init__(self, submission: Submission):
-        model = ...
+        model = GCN()
+        self.loss_fn = torch.nn.CrossEntropyLoss()
         super().__init__(model, submission)
-        self.loss_fn = ...
 
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.model(x)
+    def forward(self, data: torch.Tensor, mode="train") -> torch.Tensor:
+        x, edge_index = data.x, data.edge_index
+        x = self.model(x, edge_index)
 
-    def training_step(self, batch, batch_idx) -> torch.Tensor:
-        return NotImplementedError
+        # Only calculate the loss on the nodes corresponding to the mask
+        if mode == "train":
+            mask = data.train_mask
+        elif mode == "val":
+            mask = data.val_mask
+        elif mode == "test":
+            mask = data.test_mask
+        else:
+            assert False, "Unknown forward mode: %s" % mode
+        
+        loss = self.loss_fn(x[mask], data.y[mask])
+        acc = (x[mask].argmax(dim=-1) == data.y[mask]).sum().float() / mask.sum()
+        return loss, acc
+
+    def training_step(self, batch, batch_idx):
+        loss, acc = self.forward(batch, mode="train")
+        self.log("train_loss", loss)
+        self.log("train_acc", acc)
+        return loss
 
     def validation_step(self, batch, batch_idx):
-        raise NotImplementedError
+        _, acc = self.forward(batch, mode="val")
+        self.log("val_acc", acc)
 
     def test_step(self, batch, batch_idx):
-        raise NotImplementedError
+        _, acc = self.forward(batch, mode="test")
+        self.log("test_acc", acc)
 
     def get_specs(self) -> RuntimeSpecs:
         raise NotImplementedError
 
 class GCN(torch.nn.Module):
-    def __init__(self, hidden_channels):
+    def __init__(self, hidden_channels=16):
         super().__init__()
         # torch.manual_seed(1234567)
         
