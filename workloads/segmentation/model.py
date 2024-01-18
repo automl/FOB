0a057e7054086fce2bf911776e7724f38fba959c
Simon Blauth <simon.blauth@mailbox.org>
2024-01-18 11:36:14 +0100

    segmentation task eval

--- workloads/segmentation/model.py
+++ workloads/segmentation/model.py
@@ -1,4 +1,6 @@
+import evaluate
 import torch
+from torch.nn.functional import interpolate
 from transformers import AutoConfig, AutoModelForSemanticSegmentation
 from workloads import WorkloadModel
 from runtime.specs import RuntimeSpecs
@@ -14,7 +16,7 @@ class SegmentationModel(WorkloadModel):
         config = AutoConfig.from_pretrained("nvidia/mit-b0")
         model = AutoModelForSemanticSegmentation.from_config(config)
         super().__init__(model, submission)
-        self.loss_fn = torch.nn.CrossEntropyLoss()
+        self._reset_metric()
 
     def forward(self, x):
         return self.model(**x)
@@ -27,27 +29,44 @@ class SegmentationModel(WorkloadModel):
 
     def validation_step(self, batch, batch_idx):
         outputs = self.forward(batch)
-        loss = outputs.loss
-        self.log("val_loss", loss)
+        self._process_batch_metric(batch["labels"], outputs.logits)
+        self.log("val_loss", outputs.loss)
 
     def test_step(self, batch, batch_idx):
         outputs = self.forward(batch)
-        loss = outputs.loss
-        self.log("test_loss", loss)
+        self._process_batch_metric(batch["labels"], outputs.logits)
 
-    def compute_and_log_acc(self, preds: torch.Tensor, targets: torch.Tensor, log_label: str) -> torch.Tensor:
-        raise NotImplementedError("TODO")
+    def _process_batch_metric(self, labels: torch.Tensor, logits: torch.Tensor):
+        preds = interpolate(
+            logits,
+            size=labels.shape[-2:],
+            mode="bilinear",
+            align_corners=False
+        ).argmax(dim=1)
+        self.metric.add_batch(predictions=preds, references=labels)
 
-    def compute_and_log_loss(self, preds: torch.Tensor, targets: torch.Tensor, log_label: str) -> torch.Tensor:
-        loss = self.loss_fn(preds, targets)
-        self.log(log_label, loss)
-        return loss
+    def _reset_metric(self):
+        self.metric = evaluate.load("mean_iou")
 
     def get_specs(self) -> RuntimeSpecs:
         return RuntimeSpecs(
             max_epochs=26,
             max_steps=None,
             devices=1,  # TODO: correct devices (4)
-            target_metric="val_loss",  # TODO: correct metric (IoU)
-            target_metric_mode="min"
+            target_metric="val_mean_accuracy",
+            target_metric_mode="max"
         )
+
+    def _compute_and_log_metrics(self, stage: str):
+        metrics = self.metric.compute(num_labels=150, ignore_index=255, reduce_labels=False)
+        if not metrics is None:
+            for metric in metrics.keys():
+                if metric.startswith("mean"):
+                    self.log(f"{stage}_{metric}", metrics[metric])
+        self._reset_metric()
+
+    def on_validation_epoch_end(self) -> None:
+        self._compute_and_log_metrics("val")
+
+    def on_test_epoch_end(self) -> None:
+        self._compute_and_log_metrics("test")
