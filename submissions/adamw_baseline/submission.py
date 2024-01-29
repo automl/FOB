d7c47bbaad6f368e6ebfcf949c1e710da564a874
Simon Blauth <simon.blauth@mailbox.org>
2024-01-29 23:01:18 +0100

    add eta_min_factor to hyperparameters

--- submissions/adamw_baseline/submission.py
+++ submissions/adamw_baseline/submission.py
@@ -14,7 +14,12 @@ def get_submission(runtime_args: RuntimeArgs) -> Submission:
     return AdamWBaseline(runtime_args.hyperparameter_path)
 
 
-def cosine_warmup(step_hint: int, warmup_factor: float, optimizer):
+def cosine_warmup(
+        step_hint: int,
+        warmup_factor: float,
+        eta_min: float,
+        optimizer
+    ) -> SequentialLR | CosineAnnealingLR:
     if step_hint < 1:
         raise ValueError("step hint should be at least 1!")
     warmup_steps = math.ceil(warmup_factor * step_hint)
@@ -24,7 +29,7 @@ def cosine_warmup(step_hint: int, warmup_factor: float, optimizer):
     warmup = LinearLR(
         optimizer, start_factor=1e-10, end_factor=1., total_iters=warmup_steps)
     cosine_steps = max(step_hint - warmup_steps, 1)
-    cosine_decay = CosineAnnealingLR(optimizer, T_max=cosine_steps)
+    cosine_decay = CosineAnnealingLR(optimizer, T_max=cosine_steps, eta_min=eta_min)
     return SequentialLR(
         optimizer, schedulers=[warmup, cosine_decay], milestones=[warmup_steps])
 
@@ -33,18 +38,20 @@ class AdamWBaseline(Submission):
 
     def configure_optimizers(self, model: GroupedModel, workload_specs: SubmissionSpecs) -> OptimizerLRScheduler:
         hparams = self.get_hyperparameters()
-        parameter_groups = model.grouped_parameters(lr=hparams["learning_rate"], weight_decay=hparams["weight_decay"])
+        lr=hparams["learning_rate"]
+        weight_decay=hparams["weight_decay"]
+        parameter_groups = model.grouped_parameters(lr=lr, weight_decay=weight_decay)
         optimizer = AdamW(
             parameter_groups,
-            lr=hparams["learning_rate"],
+            lr=lr,
             eps=1e-8,
             betas=(1.0 - hparams["one_minus_beta1"], hparams["beta2"]),
-            weight_decay=hparams["weight_decay"],
+            weight_decay=weight_decay,
             fused=False
         )
         step_hint = workload_specs.max_steps if workload_specs.max_steps else workload_specs.max_epochs
         interval = "step" if workload_specs.max_steps else "epoch"
-        scheduler = cosine_warmup(step_hint, hparams["warmup_factor"], optimizer)
+        scheduler = cosine_warmup(step_hint, hparams["warmup_factor"], hparams["eta_min_factor"]*lr, optimizer)
         return {
             "optimizer": optimizer,
             "lr_scheduler": {
--- submissions/sgd_baseline/submission.py
+++ submissions/sgd_baseline/submission.py
@@ -15,16 +15,18 @@ class SGDBaseline(Submission):
 
     def configure_optimizers(self, model: GroupedModel, workload_specs: SubmissionSpecs) -> OptimizerLRScheduler:
         hparams = self.get_hyperparameters()
+        lr=hparams["learning_rate"]
+        weight_decay=hparams["weight_decay"]
         optimizer = SGD(
-            params=model.grouped_parameters(lr=hparams["learning_rate"], weight_decay=hparams["weight_decay"]),
-            lr=hparams["learning_rate"],
+            params=model.grouped_parameters(lr=lr, weight_decay=weight_decay),
+            lr=lr,
             momentum=hparams["momentum"],
-            weight_decay=hparams["weight_decay"],
+            weight_decay=weight_decay,
             nesterov=hparams["nesterov"]
         )
         step_hint = workload_specs.max_steps if workload_specs.max_steps else workload_specs.max_epochs
         interval = "step" if workload_specs.max_steps else "epoch"
-        scheduler = CosineAnnealingLR(optimizer, T_max=step_hint)
+        scheduler = CosineAnnealingLR(optimizer, T_max=step_hint, eta_min=hparams["eta_min_factor"]*lr)
         return {
             "optimizer": optimizer,
             "lr_scheduler": {
