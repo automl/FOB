8e44a227aaef1992813b3e36ca990b1ffadb2378
Simon Blauth <simon.blauth@mailbox.org>
2024-03-01 18:17:07 +0100

    parse extra args

--- runtime/runtime.py
+++ runtime/runtime.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from typing import Any, Callable
+from typing import Any, Callable, Iterable
+import re
 import yaml
 from submissions import submission_path
 from workloads import workload_path
@@ -12,22 +13,40 @@ def runtime_path() -> Path:
 
 class Runtime():
     def __init__(self) -> None:
-        self.experiments = []
+        self.runs = []
         self.workload_key = "workload"
         self.submission_key = "submission"
         self.identifier_key = "name"
         self.default_file_name = "default.yaml"
 
-    def parse_config(self, file: Path):
+    def parse_experiment(self, file: Path, extra_args: Iterable[str] = tuple()):
         searchspace = self._parse_yaml(file)
-        self.experiments += gridsearch(searchspace)
+        for arg in extra_args:
+            self._parse_into_searchspace(searchspace, arg)
+        self.runs += gridsearch(searchspace)
+
+    def _parse_into_searchspace(self, searchspace: dict[str, Any], arg: str):
+        keys, value = arg.split("=")
+        keys = keys.split(".")
+        keys_with_list_indices = []
+        for key in keys:
+            match = re.search(r"^(.*?)\[(\-?\d+)\]$", key)
+            if match:
+                keys_with_list_indices.append(match.group(1))
+                keys_with_list_indices.append(int(match.group(2)))
+            else:
+                keys_with_list_indices.append(key)
+        target = searchspace
+        for key in keys_with_list_indices[:-1]:
+            target = target[key]
+        target[keys_with_list_indices[-1]] = value
 
     def fill_experiments_from_default(self):
-        for i, _ in enumerate(self.experiments):
+        for i, _ in enumerate(self.runs):
             # order from higher to lower in hierarchy
-            self.experiments[i] = self._fill_unnamed_from_default(self.experiments[i], runtime_path)
-            self.experiments[i] = self._fill_named_from_default(self.experiments[i], self.workload_key, workload_path)
-            self.experiments[i] = self._fill_named_from_default(self.experiments[i], self.submission_key, submission_path)
+            self.runs[i] = self._fill_unnamed_from_default(self.runs[i], runtime_path)
+            self.runs[i] = self._fill_named_from_default(self.runs[i], self.workload_key, workload_path)
+            self.runs[i] = self._fill_named_from_default(self.runs[i], self.submission_key, submission_path)
 
     def _fill_unnamed_from_default(self, experiment: dict[str, Any], unnamed_root: Callable) -> dict[str, Any]:
         default_path: Path = unnamed_root() / self.default_file_name
@@ -64,7 +83,7 @@ class Runtime():
 
     def dump_experiments(self):
         # TODO: remove this and make proper export function
-        for i, e in enumerate(self.experiments):
+        for i, e in enumerate(self.runs):
             outpath = Path(e["runtime"]["output_dir"]) / f"experiment_{i}.yaml"
             outpath.parent.mkdir(parents=True, exist_ok=True)
             with open(outpath, "w", encoding="utf8") as f:
