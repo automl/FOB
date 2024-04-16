ba54e39d56fc5c4dfbde83c34864c87a6902a83d
Simon Blauth <simon.blauth@mailbox.org>
2024-04-16 19:41:27 +0200

    add reference yaml for graph_tiny

--- tasks/graph_tiny/model.py
+++ tasks/graph_tiny/model.py
@@ -1,11 +1,12 @@
 import torch
+import torch.nn.functional as F
+from torch import nn
+from torch import Tensor
+from torch_geometric.nn import GCNConv
+import torch_geometric.data as geom_data
 from tasks import TaskModel
 from engine.configs import TaskConfig
 from optimizers import Optimizer
-from torch_geometric.nn import GCNConv
-from torch import nn
-import torch.nn.functional as F
-from tasks.graph_tiny.gcn import GCN as OurGCN
 
 
 class CoraModel(TaskModel):
@@ -18,29 +19,19 @@ class CoraModel(TaskModel):
         normalize = config.model.normalize
         dropout = config.model.dropout
         reset_params = config.model.reset_params
-        model_name = config.model.name
-        if model_name == "GCN":
-            model = GCN(hidden_channels=hidden_channels,
-                    num_layers=num_layers,
-                    dropout=dropout,
-                    cached=cached,
-                    normalize=normalize,
-                    reset_params=reset_params)
-        elif model_name == "our_GCN":
-            if num_layers != 2:
-                # also ignoring cached, normalize, reset_params from config atm
-                raise NotImplementedError()
-            nfeat = 1433
-            nclass = 7
-            model = OurGCN(nfeat=nfeat,
-                           nhid=hidden_channels,
-                           nclass=nclass,
-                           dropout=dropout)
+        model = GCN(
+            hidden_channels=hidden_channels,
+            num_layers=num_layers,
+            dropout=dropout,
+            cached=cached,
+            normalize=normalize,
+            reset_params=reset_params
+        )
 
         super().__init__(model, optimizer, config)
-        self.loss_fn = torch.nn.CrossEntropyLoss()
+        self.loss_fn = nn.CrossEntropyLoss()
 
-    def forward(self, data: torch.Tensor, mode="train") -> torch.Tensor:
+    def forward(self, data: geom_data.data.BaseData, mode="train") -> tuple[Tensor, Tensor]:
         x, edge_index = data.x, data.edge_index
         x = self.model(x, edge_index)
 
@@ -52,7 +43,7 @@ class CoraModel(TaskModel):
         elif mode == "test":
             mask = data.test_mask
         else:
-            assert False, "Unknown forward mode: %s" % mode
+            assert False, f"Unknown forward mode: {mode}"
 
         loss = self.loss_fn(x[mask], data.y[mask])
         acc = (x[mask].argmax(dim=-1) == data.y[mask]).sum().float() / mask.sum()
@@ -130,10 +121,10 @@ class GCN(torch.nn.Module):
             for param in conv.parameters():
                 if param.dim() > 1:  # weight parameter
                     stdv = 1. / torch.sqrt(torch.tensor(param.size(1)))
-                    torch.nn.init.uniform_(param, -stdv, stdv)
+                    nn.init.uniform_(param, -stdv.item(), stdv.item())
                 else:  # bias
                     stdv = 1. / torch.sqrt(torch.tensor(param.size(0)))
-                    torch.nn.init.uniform_(param, -stdv.item(), stdv.item())
+                    nn.init.uniform_(param, -stdv.item(), stdv.item())
 
     def forward(self, x, edge_index):
         for idx, conv in enumerate(self.convs):
