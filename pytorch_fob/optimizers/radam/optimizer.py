109d1feb4092be5701e1f9d93e0df62514e252a1
Tobias Bürger <tobi-buerger@web.de>
2024-07-16 18:17:57 +0200

    add RAdam optimizer

---
+++ pytorch_fob/optimizers/radam/optimizer.py
@@ -0,0 +1,26 @@
+from radam import RAdam
+from lightning.pytorch.utilities.types import OptimizerLRScheduler
+from pytorch_fob.engine.configs import OptimizerConfig
+from pytorch_fob.engine.parameter_groups import GroupedModel
+from pytorch_fob.optimizers.lr_schedulers import get_lr_scheduler
+
+
+def configure_optimizers(model: GroupedModel, config: OptimizerConfig) -> OptimizerLRScheduler:
+    lr = config.learning_rate
+    weight_decay = config.weight_decay
+    parameter_groups = model.grouped_parameters(lr=lr, weight_decay=weight_decay)
+    optimizer = RAdam(
+        parameter_groups,
+        lr=lr,
+        eps=config.epsilon,
+        betas=(config.beta1, config.beta2),
+        weight_decay=weight_decay,
+    )
+    lr_scheduler = get_lr_scheduler(optimizer, config)
+    return {
+        "optimizer": optimizer,
+        "lr_scheduler": {
+            "scheduler": lr_scheduler,
+            "interval": config.lr_scheduler.interval
+        }
+    }
